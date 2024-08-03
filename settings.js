5ff63ec8b0596d969216a3d09a0f3332f4b5501f
Chandralonda <londarezky@gmail.com>
2024-08-04 07:12:15 +0800

    chandra

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6285863252864']
+global.botname = 'chandra'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Chandra"
+global.sticker2 = "🌜"
